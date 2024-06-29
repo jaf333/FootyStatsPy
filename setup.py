26ad4be7594a1972af3d93db67eb14b09a185c90
jesus <jantolin@inhub.es>
2024-06-29 12:26:16 +0200

    setup.py done

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='FootyStatsPy',
+    version='0.1.0',
+    description='A Python library for football data scraping and visualization',
+    author='Jesús Antolín Felipe',
+    author_email='jaf.attd@gmail.com',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'beautifulsoup4',
+        'pandas',
+        'mplsoccer',
+        'matplotlib',
+        'Pillow'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
+
