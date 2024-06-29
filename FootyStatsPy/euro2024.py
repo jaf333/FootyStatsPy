90c08ccfe14481a766508326386240e76d75b9b2
jesus <jantolin@inhub.es>
2024-06-29 13:18:58 +0200

    fixed scraping functionality for Euro 2024 data

--- FootyStatsPy/euro2024.py
+++ FootyStatsPy/euro2024.py
@@ -7,7 +7,6 @@ def get_columns_df(r, table_name):
     lst = []  # To append columns' names
     soup = BeautifulSoup(r.content, "html.parser")
     tables = soup.find_all("table")
-
     for table in tables:
         if table.get('id') and str(table.get('id')).endswith(table_name):
             for row in table.tbody.find_all('tr'):
@@ -23,34 +22,29 @@ def get_columns_df(r, table_name):
                     data_stat = col.get("data-stat")
                     if data_stat and data_stat not in lst:
                         lst.append(str(data_stat))
-
     return pd.DataFrame(columns=lst)
 
 def get_data_fbref(url, table_name):
-    session = requests.Session()
-    rq = session.get(url)
-    soup = BeautifulSoup(rq.content, "html.parser")
+    rq = requests.session().get(url)
+    soup = BeautifulSoup(rq.text, "html.parser")
     index = 0
     df = get_columns_df(rq, table_name)
     tables = soup.find_all("table")
 
     for table in tables:
         if table_name == "shots_all" and table.get('id') == "shots_all":
-            print(f"Found table with id 'shots_all'")
             for row in table.tbody.find_all('tr'):
                 columns = row.find_all('td')
-                row_data = [col.text for col in columns]
+                row_data = [columns[j].text for j in range(len(columns))]
                 df.loc[len(df)] = row_data
 
         elif table_name == "keeper" and "keeper" in str(table.get('id')):
-            print(f"Found keeper table with id {table.get('id')}")
             for row in table.tbody.find_all('tr'):
                 columns = row.find_all('td')
-                row_data = [col.text for col in columns]
+                row_data = [columns[j].text for j in range(len(columns))]
                 df.loc[len(df)] = row_data
 
         elif table.get('id') and str(table.get('id')).endswith(table_name):
-            print(f"Found table with id {table.get('id')}")
             for row in table.tbody.find_all('tr'):
                 names = row.find_all('th')
                 player_name = None
@@ -65,7 +59,7 @@ def get_data_fbref(url, table_name):
                         df.iloc[index, j] = columns[j].text
                 index += 1
 
-    df["match"] = url.split("/")[-2]
+    df["match"] = url.split("/")[6]
     return df
 
 def scrape_euro2024_data():
