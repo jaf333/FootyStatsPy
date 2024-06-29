dd30fdb881b5b6a39197cee81722dc04bd022297
jesus <jantolin@inhub.es>
2024-06-29 13:38:47 +0200

    adjustments on euro2024 scrape

--- FootyStatsPy/euro2024.py
+++ FootyStatsPy/euro2024.py
@@ -2,10 +2,35 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 import time
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
 
-def get_columns_df(r, table_name):
-    lst = []  # To append columns' names
-    soup = BeautifulSoup(r.content, "html.parser")
+# ChromeDriver
+# ChromeDriver 路径
+chrome_driver_path = "/Users/devdev/Desktop/SIDEPROJECTS/FootyStatsPy/chromedriver"
+
+# Configurar ChromeDriver
+service = Service(executable_path=chrome_driver_path)
+
+
+options = Options()
+options.add_experimental_option("excludeSwitches", ["enable-automation"])
+options.add_experimental_option('useAutomationExtension', False)
+
+
+driver = webdriver.Chrome(service=service, options=options)
+
+
+
+
+# Función para obtener las columnas del DataFrame
+def get_columns_df(soup, table_name):
+    lst = []  # Para almacenar los nombres de las columnas
     tables = soup.find_all("table")
     for table in tables:
         if table.get('id') and str(table.get('id')).endswith(table_name):
@@ -24,61 +49,80 @@ def get_columns_df(r, table_name):
                         lst.append(str(data_stat))
     return pd.DataFrame(columns=lst)
 
+# Función para obtener los datos de fbref
 def get_data_fbref(url, table_name):
-    rq = requests.session().get(url)
-    soup = BeautifulSoup(rq.text, "html.parser")
-    index = 0
-    df = get_columns_df(rq, table_name)
-    tables = soup.find_all("table")
+    try:
+        driver.get(url)
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, "table")))
+        soup = BeautifulSoup(driver.page_source, "html.parser")
+        df = get_columns_df(soup, table_name)
+        index = 0
+        tables = soup.find_all("table")
 
-    for table in tables:
-        if table_name == "shots_all" and table.get('id') == "shots_all":
-            for row in table.tbody.find_all('tr'):
-                columns = row.find_all('td')
-                row_data = [columns[j].text for j in range(len(columns))]
-                df.loc[len(df)] = row_data
+        for table in tables:
+            if table_name == "shots_all" and table.get('id') == "shots_all":
+                for row in table.tbody.find_all('tr'):
+                    columns = row.find_all('td')
+                    row_data = [columns[j].text for j in range(len(columns))]
+                    df.loc[len(df)] = row_data
 
-        elif table_name == "keeper" and "keeper" in str(table.get('id')):
-            for row in table.tbody.find_all('tr'):
-                columns = row.find_all('td')
-                row_data = [columns[j].text for j in range(len(columns))]
-                df.loc[len(df)] = row_data
+            elif table_name == "keeper" and "keeper" in str(table.get('id')):
+                for row in table.tbody.find_all('tr'):
+                    columns = row.find_all('td')
+                    row_data = [columns[j].text for j in range(len(columns))]
+                    df.loc[len(df)] = row_data
 
-        elif table.get('id') and str(table.get('id')).endswith(table_name):
-            for row in table.tbody.find_all('tr'):
-                names = row.find_all('th')
-                player_name = None
-                for name in names:
-                    player_name = name.get('csk')
-                if player_name:
-                    df.loc[index, "name"] = player_name
-
-                columns = row.find_all('td')
-                for j in range(len(columns)):
+            elif table.get('id') and str(table.get('id')).endswith(table_name):
+                for row in table.tbody.find_all('tr'):
+                    names = row.find_all('th')
+                    player_name = None
+                    for name in names:
+                        player_name = name.get('csk')
                     if player_name:
-                        df.iloc[index, j] = columns[j].text
-                index += 1
+                        df.loc[index, "name"] = player_name
 
-    df["match"] = url.split("/")[6]
-    return df
+                    columns = row.find_all('td')
+                    for j in range(len(columns)):
+                        if player_name:
+                            df.iloc[index, j] = columns[j].text
+                    index += 1
 
+        df["match"] = url.split("/")[6]
+        return df
+    except TimeoutException:
+        print(f"Timeout while accessing {url}")
+        return pd.DataFrame()
+    except Exception as e:
+        print(f"Error while scraping {url}: {e}")
+        return pd.DataFrame()
+
+# Función principal para scrape_euro2024_data
 def scrape_euro2024_data():
     urls = ["https://fbref.com/en/comps/676/schedule/European-Championship-Scores-and-Fixtures"]
     table_ids = ["stats", "keepers", "shots_all"]
     data_dict = {}
-    i = 0
 
     for url in urls:
         for table_id in table_ids:
-            response = requests.get(url)
-            if response.status_code == 200:
-                print(f"Scraping data for {table_id} from {url}")
-                data_dict[f"{table_id}_euro2024"] = get_data_fbref(url, table_id)
-                i += 1
-                time.sleep(5)  # Sleep to avoid being blocked
+            print(f"Scraping data for {table_id} from {url}")
+            df = get_data_fbref(url, table_id)
+            if not df.empty:
+                data_dict[f"{table_id}_euro2024"] = df
+                time.sleep(5)  # Dormir para evitar ser bloqueado
             else:
-                print(f"Failed to access {url}. Retrying after a delay.")
+                print(f"Failed to scrape data for {table_id} from {url}. Retrying after a delay.")
                 time.sleep(300)
 
-    final_df = pd.concat(data_dict.values(), ignore_index=True)
-    return final_df
+    if data_dict:
+        final_df = pd.concat(data_dict.values(), ignore_index=True)
+        return final_df
+    else:
+        return pd.DataFrame()
+
+if __name__ == '__main__':
+    data = scrape_euro2024_data()
+    if not data.empty:
+        data.to_csv("euro2024_data.csv", index=False)
+        print("Data scraped successfully and saved to euro2024_data.csv")
+    else:
+        print("Failed to scrape any data.")
