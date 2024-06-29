c936669f91bf2118eec3264ca204488e7ecde41f
jesus <jantolin@inhub.es>
2024-06-29 13:08:34 +0200

    add unit tests for euro2024 module

---
+++ tests/test_euro2024.py
@@ -0,0 +1,13 @@
+import unittest
+from MyFootballStats.euro2024 import scrape_euro2024_data
+
+class TestEuro2024(unittest.TestCase):
+
+    def test_scrape_euro2024_data(self):
+        df = scrape_euro2024_data()
+        self.assertIsInstance(df, pd.DataFrame)
+        self.assertFalse(df.empty)
+
+if __name__ == '__main__':
+    unittest.main()
+
